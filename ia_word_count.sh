c213330ec8bfd4ec2a16a2eb9d70aa0045d5202b
Eric C. Weig <eweig@email.uky.edu>
2017-02-25 15:46:33 -0500

    Update ia_word_count.sh

--- ia_word_count.sh
+++ ia_word_count.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 # @copyright Copyright 2017 Eric C. Weig 
 
-arkid=$1
+#read commandline variables
 
+arkid=$1
 coll=$2
 
+#set some needed variables
 meta="_meta.xml"
-
 text="_djvu.txt"
-
 words="_words.txt"
-
 space="_"
-
 destdir=/var/opt/iawordcounts
 
+#grab needed file from the Internet Archive
 wget "https://archive.org/download/$arkid/$arkid$text"
-
 wget -O "$destdir/date.txt" "http://archive.org/advancedsearch.php?q=$arkid&fl%5B%5D=date&page=1&output=json"
 
+#parse the JSON date file to extract only the date and store as variable
 sed -i -E "s/.*docs//" $destdir/date.txt
 sed -i -E "s/.*:\"//" $destdir/date.txt
 sed -i -E "s/T00:00:00Z.*//" $destdir/date.txt
@@ -27,8 +26,9 @@ sed -i -E "s/-//" $destdir/date.txt
 
 date=$(<$destdir/date.txt)
 
+#count the number of words in the djvu file and save this as a file
 wc -w < $destdir/$arkid$text > $destdir/$coll/$coll$space$date$space$arkid$words
 
+#cleanup by removing files no longer needed
 rm $destdir/$arkid$text
-
 rm $destdir/date.txt
