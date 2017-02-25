33fbb8500b2d0b18e48b95f47def21bbb1f8cab7
Eric C. Weig <eweig@email.uky.edu>
2017-02-25 15:31:38 -0500

    Add files via upload

---
+++ ia_word_count.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# @copyright Copyright 2017 Eric C. Weig 
+
+arkid=$1
+
+coll=$2
+
+meta="_meta.xml"
+
+text="_djvu.txt"
+
+words="_words.txt"
+
+space="_"
+
+destdir=/var/opt/iawordcounts
+
+wget "https://archive.org/download/$arkid/$arkid$text"
+
+wget -O "$destdir/date.txt" "http://archive.org/advancedsearch.php?q=xt7jsx645m27&fl%5B%5D=date&page=1&output=json"
+
+sed -i -E "s/.*docs//" $destdir/date.txt
+sed -i -E "s/.*:\"//" $destdir/date.txt
+sed -i -E "s/T00:00:00Z.*//" $destdir/date.txt
+sed -i -E "s/-//" $destdir/date.txt
+sed -i -E "s/-//" $destdir/date.txt
+
+date=$(<$destdir/date.txt)
+
+wc -w < $destdir/$arkid$text > $destdir/$coll/$coll$space$date$space$arkid$words
+
+rm $destdir/$arkid$text
+
+rm $destdir/date.txt
